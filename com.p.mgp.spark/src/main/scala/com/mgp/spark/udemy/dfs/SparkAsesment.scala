9be068d34552c1f266582b729cfe557e198ee362
mgpradeepa <mgpradeepa@gmail.com>
2018-06-30 18:12:02 +0530

    Use case in execution

--- com.p.mgp.spark/src/main/scala/com/mgp/spark/udemy/dfs/SparkAsesment.scala
+++ com.p.mgp.spark/src/main/scala/com/mgp/spark/udemy/dfs/SparkAsesment.scala
@@ -1,5 +1,6 @@
 package com.mgp.spark.udemy.dfs
 
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 /**
   * Created by mgpradeepa on 30/06/18.
@@ -7,11 +8,14 @@ import org.apache.spark.sql.SparkSession
 object SparkAsesment {
   def main(args: Array[String]): Unit = {
 
-    val spark = SparkSession.builder().getOrCreate()
+    val spark = SparkSession.builder().master("local").appName("Usecase1").getOrCreate()
+    ///Users/mgpradeepa/Public/mgp_gitter/spark_git/spark_uc/com.p.mgp.spark/src/main/resources
 
-    val df = spark.read.option("header","true").option("inferSchema","true").csv("resources/Netflix.csv")
+    val df = spark.read.option("header","true").option("inferSchema","true").csv("/Users/mgpradeepa/Public/mgp_gitter/spark_git/spark_uc/com.p.mgp.spark/src/main/resources/Netflix.csv")
 
     df.columns
+    df.show()
+
   }
 
 
